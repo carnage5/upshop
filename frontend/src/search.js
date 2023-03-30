827b2ae227bfdb8715bde55ee9b31983eb2aefb3
carnage5 <abishek.deivam@gmail.com>
2023-03-30 20:46:04 +0530

    search frontend done

--- frontend/src/search.js
+++ frontend/src/search.js
@@ -3,9 +3,14 @@ import Display from "./display";
 
 function Search() {
     const [query,setquery]=useState("")
+    const [qsearch,setqsearch]=useState(false)
     const [plist,setplist]= useState([])
     const getproducts=async()=>{
-        const res = await fetch('http://localhost:5002/product')
+        var res
+        if(qsearch)
+            { res=await fetch('http://localhost:5002/product/'+query)}
+        else
+            { res = await fetch('http://localhost:5002/product')}
         const json = await res.json()
         if(res.ok)
             setplist([...json])
@@ -17,6 +22,8 @@ function Search() {
     }
     const handlesubmit=(event)=>{
         event.preventDefault()
+        setqsearch(true)
+        getproducts()
         console.log(query)
     }
     useEffect(() => {
@@ -31,7 +38,7 @@ function Search() {
                 <div class=" absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
                     <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
                 </div>
-                <input type="search" onChange={changeval} value={query} class=" block w-full p-4 pl-10 text-xl text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-red-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500" placeholder="Search for ..." required/>
+                <input type="search" onChange={changeval} value={query} class=" block w-full p-4 pl-10 text-xl text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-red-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500" placeholder="Search for ..." />
                 <button type="submit" class="text-white absolute right-2.5 bottom-2.5 my-1 bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">Search</button>
             </div>
         </form>
