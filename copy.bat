2ee622bcfc156399d2c00df73b49464c38e4c3f6
abishekd <79302423+carnage5@users.noreply.github.com>
2023-04-19 14:13:28 +0530

    Update copy.bat

--- copy.bat
+++ copy.bat
@@ -1,8 +1,5 @@
 copy C:\Users\Abishek\Desktop\cc\cc_project\uc1\.env C:\Users\Abishek\.jenkins\workspace\cc-project-pipeline\uc1
-yes
 copy C:\Users\Abishek\Desktop\cc\cc_project\uc2\.env C:\Users\Abishek\.jenkins\workspace\cc-project-pipeline\uc2
-yes
 copy C:\Users\Abishek\Desktop\cc\cc_project\uc3\.env C:\Users\Abishek\.jenkins\workspace\cc-project-pipeline\uc3
-yes
 
 
